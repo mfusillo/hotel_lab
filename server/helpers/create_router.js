6f60255791c11c5e1e0a6bb2499d4be1ec7d68a4
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-22 11:22:24 +0000

    add update and delete routes

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -28,6 +28,29 @@ const createRouter = function (collection) {
     })
   })
 
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    collection.deleteOne({ _id: ObjectID(id) })
+    // .then( () => { collection.find().toArray() } )
+    .then(result => res.json(result.result))
+    .catch(err => {
+      console.error(err)
+      res.status(500)
+      res.json({ status: 500, error:err })
+    })
+  })
+
+  router.put('/:id', (req, res) =>{
+    const id = req.params.id
+    collection.findOneAndUpdate({ _id: ObjectID(id) }, { $set: req.body }, {returnOriginal: false})
+    .then(result => res.json(result.value))
+    .catch(err => {
+      console.error(err)
+      res.status(500)
+      res.json({ status: 500, error:err })
+    })
+  })
+
 
   return router
 
